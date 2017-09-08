92969e0375f3831f8ab523419696656bd26d0eac
Brian Fajardo <bmf5@hotmail.com>
2017-09-08 19:29:12 -0400

    Input, render messages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,36 @@ import ReactDOM from 'react-dom'
 
 class App extends Component {
 
+  constructor() {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = { messages: [] }
+  }
+
+  handleSubmit(e) {
+    const text = e.target.value
+    const message = { text, from: 'Me' }
+
+    if (message && e.keyCode === 13) {
+      this.setState({ messages: [...this.state.messages, message] })
+      e.target.value = ''
+    }
+  }
+
   render() {
+    const messages = this.state.messages.map(({ text, from }, index) => {
+      return <li key={index}>{from}: {text}</li>
+    })
+
     return (
-      <h1>Hello world!</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a message..."
+          onKeyUp={this.handleSubmit}
+        />
+        <ul>{messages}</ul>
+      </div>
     )
   }
 }
